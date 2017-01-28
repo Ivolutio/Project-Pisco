3cde2a127a9b30a81f5a17b103c5ac88195a1559
Pixel / IKStreamIvo <ivostreaming@gmail.com>
2017-01-28 14:42:25 +0100

    Added Player movement and tile prefabs

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class PlayerControls : MonoBehaviour {
+
+    public float speed = .05f;
+
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void FixedUpdate()
+    {
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(movex * speed, movey * speed);
+    }
 }
